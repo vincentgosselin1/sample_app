090151596ffc8544348304b5e45310df410810bc
vincentgosselin1 <vincentgosselin1@gmail.com>
2016-08-25 16:32:04 -0400

    Static pages avec Dynamic title

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe PagesController, type: :controller do
       get :home
       expect(response).to have_http_status(:success)
     end
+
+    it "should have the right title" do
+      get :home
+      assert_select "title", "Ruby on Rails Tutorial Sample App | Home"
+    end
   end
 
   describe "GET #contact" do
@@ -15,12 +20,22 @@ RSpec.describe PagesController, type: :controller do
       get :contact
       expect(response).to have_http_status(:success)
     end
+
+    it "should have the right title" do
+      get :contact
+      assert_select "title", "Ruby on Rails Tutorial Sample App | Contact"
+    end
   end
 
   describe "GET #about" do
     it "returns http success" do
       get :about
       expect(response).to have_http_status(:success)
+    end
+
+    it "should have the right title" do
+      get :about
+      assert_select "title", "Ruby on Rails Tutorial Sample App | About"
     end
   end
 
